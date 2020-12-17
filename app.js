7cd93705c863e48666c896244d0748ce9c9b51a9
Rae Stichter <raestichter@gmail.com>
2020-12-16 22:29:12 -0500

    initial capture information

--- app.js
+++ app.js
@@ -11,8 +11,8 @@
 // var commandLineArgs = process.argv;
 // console.log(commandLineArgs);
 
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+//console.log(profileDataArgs);
 
 // const message = 'Hello Node!';
 // message = 'Goodbye Node!';
@@ -20,14 +20,47 @@
 // const sum = 5 + 3;
 // sum += 1;
 
-const animalArray = ['dog', 'cat', 'pig'];
+// const animalArray = ['dog', 'cat', 'pig'];
 
-animalArray.push('cow');
+// animalArray.push('cow');
 
-const personObj = {
-  name: 'Lernantino',
-  age: 99
+// const personObj = {
+//   name: 'Lernantino',
+//   age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+
+
+// one way of doing this.  But we want to try using the arrow functions
+// const printProfileData = (profileDataArr) => {
+//   console.log(profileDataArr);
+// };
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+// };
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  // profileDataArr.forEach((profileItem) => {
+  //   console.log(profileItem)
+  // });
+  // this is the same as the above function set up, but instead setup with the arrow function
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-personObj.age = 100;
-personObj.occupation = 'Developer';
+
+printProfileData(profileDataArgs);
