f2b6481c1c7087ebc098ec232cdf6f683d018dc4
Rae Stichter <raestichter@gmail.com>
2021-01-03 21:09:58 -0500

    optimized app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 // var commandLineArgs = process.argv;
 // console.log(commandLineArgs);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
 // const message = 'Hello Node!';
@@ -46,21 +46,58 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 //   }
 // };
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
 
-  console.log('================');
+//   console.log('================');
 
-  // Is the same as this...
-  // profileDataArr.forEach((profileItem) => {
-  //   console.log(profileItem)
-  // });
-  // this is the same as the above function set up, but instead setup with the arrow function
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//   // Is the same as this...
+//   // profileDataArr.forEach((profileItem) => {
+//   //   console.log(profileItem)
+//   // });
+//   // this is the same as the above function set up, but instead setup with the arrow function
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+//printProfileData(profileDataArgs);
 
+// -------------------------------------- Lesson 1 above, below provided what we should move forward with--------------
+// const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+// -------------lesson 2 -------------------
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// saving the inputs that are entered into the array into variables
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// make this into one line:
+const [name, github] = profileDataArgs;
+
+// first arguement is the file name that will be created.  the second is the data
+// that is being written (html string template).  The third is the callback function
+// that will handle any errors as well as a success message.
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
